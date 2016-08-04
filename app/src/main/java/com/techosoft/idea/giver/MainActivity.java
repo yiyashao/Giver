ac8bd1e73d77155a3caa8e0c1cfc49c242f10969
David <yiyashao@gmail.com>
2016-08-04 21:30:59 +0800

    something not working but lets call it for tonight

--- app/src/main/java/com/techosoft/idea/giver/MainActivity.java
+++ app/src/main/java/com/techosoft/idea/giver/MainActivity.java
@@ -1,8 +1,14 @@
 package com.techosoft.idea.giver;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("Test: ","Gets to the ON CREATE");
+        //declear the buttons and setup listeners
+        final Button button = (Button) findViewById(R.id.btnGive);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked the button", Toast.LENGTH_SHORT).show();
+                goToWantForm();
+            }
+        });
+
+//        naviWant = (Button) findViewById(R.id.btnWant);
+//        naviWant.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                Log.d("Test: ","Gets to the listener");
+//                goToWantForm();
+//            }
+//        });
+        naviGive = (Button) findViewById(R.id.btnWant);
+        naviGive.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                goToGiveForm();
+            }
+        });
 
     }
+
+
+    //handle the listener actions
+    private void goToWantForm() {
+        Log.d("Test: ","Enters the form call");
+        Intent intent = new Intent(this, FormWant.class);
+        startActivity(intent);
+    }
+
+    private void goToGiveForm() {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+    }
+
+
 }
