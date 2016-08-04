4e786714b72a198e4f4aa90fc9070030e234dc24
David <yiyashao@gmail.com>
2016-08-04 19:15:14 +0800

    another change

--- app/src/main/java/com/techosoft/idea/giver/MainActivity.java
+++ app/src/main/java/com/techosoft/idea/giver/MainActivity.java
@@ -2,12 +2,18 @@ package com.techosoft.idea.giver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button naviWant;
+    Button naviGive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
